522859a50cd94f0cf0b08accf776f7af2e7c0cb7
Barney Desmond <barneydesmond@gmail.com>
2022-02-04 04:52:44 +1100

    Fix up the running uid/gid

--- Makefile
+++ Makefile
@@ -3,6 +3,8 @@ DATADIR := /root/git/moinmoin-wiki-docker/datavol
 HTTP_PORT := 8000
 RUNNING_CONTAINER_NAME := meidokon_wiki
 TZ := Australia/Sydney
+MOIN_UID := 1002
+MOIN_GID := 1002
 
 build:
 	docker build -t $(IMAGENAME) .
@@ -11,7 +13,7 @@ run:
 	-docker kill $(RUNNING_CONTAINER_NAME)
 	sleep 2
 	-docker container rm meidokon_wiki
-	docker run -e TZ=$(TZ) -d -p $(HTTP_PORT):80 -v $(DATADIR):/usr/local/share/moin/data --name $(RUNNING_CONTAINER_NAME) $(IMAGENAME)
+	docker run -e TZ=$(TZ) -e MOIN_UID=$(MOIN_UID) -e MOIN_GID=$(MOIN_GID) -d -p $(HTTP_PORT):80 -v $(DATADIR):/usr/local/share/moin/data --name $(RUNNING_CONTAINER_NAME) $(IMAGENAME)
 
 update: build run
 
--- start.sh
+++ start.sh
@@ -1,17 +1,30 @@
 #!/usr/bin/env sh
 
+# This should be passed in from docker run, useful for correct file ownership
+# in your datavol on the host.
+# -e MOIN_UID=jbloggs -e MOIN_GID=jbloggs
+UWSGI_UID="${MOIN_UID:-www-data}"
+UWSGI_GID="${MOIN_GID:-www-data}"
+
 # If the data folder does not contain file 'initialized' it is most likely because
 # the container is fresh and has been started with
 # the volume option
 if ! [ "$(ls -A /usr/local/share/moin/data/initialized 2>/dev/null)" ]; then
     cp -r /usr/local/share/moin/bootstrap-data/* /usr/local/share/moin/data/
     touch /usr/local/share/moin/data/initialized
-    chown -R www-data:www-data /usr/local/share/moin/data
+    chown -R "${UWSGI_UID}:${UWSGI_GID}" /usr/local/share/moin/data
 fi
 
+# Correct filesystem ownership if given
+# XXX: data could be very slow if there's lots of pages
+chown -R "${UWSGI_UID}:${UWSGI_GID}" /usr/local/share/moin/data
+chown -R "${UWSGI_UID}:${UWSGI_GID}" /usr/local/share/moin/underlay
+
 service rsyslog start && service nginx start && uwsgi \
-    --uid www-data \
+    --uid "${UWSGI_UID}" \
+    --gid "${UWSGI_GID}" \
     -s /tmp/uwsgi.sock \
+    --chown-socket www-data:www-data \
     --plugins python \
     --pidfile /var/run/uwsgi-moinmoin.pid \
     --wsgi-file server/moin.wsgi \
